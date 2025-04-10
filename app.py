bef8afe5ca783f53bed825ca4aba4fbc68484f63
Danagul24 <60794847+Danagul24@users.noreply.github.com>
2025-04-10 23:46:58 +0500

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ html_form = """
     </style>
 </head>
 <body>
-    <h2>Введите данные</h2>
+    <h3>Введите данные</h3>
     <form method="post">
         Качество продукта: <input name="p1" type="number" min="1" max="5" required><br>
         Качество обратной связи: <input name="p2" type="number" min="1" max="5" required><br>
